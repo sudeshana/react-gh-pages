a88ce7edee86b0b9dc716097085e6c43bafdb2b8
sudeshana <dgpradeepal@gmail.com>
2019-09-22 17:08:14 +0530

    Updates

---
+++ precache-manifest.8b6090fcd9fbf9476fc9427ece2695f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fc4824b820049d3d9bebc48d7d59ba4",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "0a8a6ef6dd954dc6d974",
+    "url": "/react-gh-pages/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "65069664b6ed55378ebe",
+    "url": "/react-gh-pages/static/js/2.5dc4a546.chunk.js"
+  },
+  {
+    "revision": "0a8a6ef6dd954dc6d974",
+    "url": "/react-gh-pages/static/js/main.9b372be8.chunk.js"
+  },
+  {
+    "revision": "5c14bf8d78ce841fcbb2",
+    "url": "/react-gh-pages/static/js/runtime-main.881c30c4.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/react-gh-pages/static/media/logo.25bf045c.svg"
+  }
+]);
